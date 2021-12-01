add353a0f8cd5f7fd1062d5297056dfe8d09ebb4
kalkidan Demes <Kalkidan.Demes@belcash.com>
2021-12-01 09:17:56 +0300

    added task1 and 2

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-    const arr = [];
-    for (const idx in array) {
-      arr.push(appendString + idx);
-    }
-  
-    return array;
-  }
--- 0x00-ES6_basic/100-createIteratorObject.js
+++
@@ -1,7 +0,0 @@
-export default function* createIteratorObject(report) {
-    for (const dept of Object.keys(report.allEmployees)) {
-      for (const employee of report.allEmployees[dept]) {
-        yield employee;
-      }
-    }
-  }
