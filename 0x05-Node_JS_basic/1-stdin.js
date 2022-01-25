f54ce763010853f028b2681ba37381c411b96369
kalkidan Demes <Kalkidan.Demes@belcash.com>
2022-01-25 12:18:31 +0300

    stdin operations

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,10 @@
-const firstLine = 'Welcome to Holberton School, what is your name?\n';
-const readLine = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-readLine.question(`${firstLine}`, (name) => {
-  console.log(`Your name is: ${name}`);
-  readLine.close();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on('end', () => {
